14defd2627f2087c24f2b6565bd2d5a1de810744
boksul <boksul@hotmail.com>
2019-04-23 18:47:41 +0000

    validation for all 4 specs added

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   before :each do
-    @category = Category.new(name: "test_category")
+    @category = Category.new(id: 1, name: "test_category")
     @product = Product.new(
       name: "test_product",
-      description: "test_description",
-      image: "none",
+      quantity: 3,
       price_cents: 100,
       category_id: @category.id
     )
   end
 
   describe 'Validations' do
+    it 'should be valid with all valid attr' do
+      expect(@product).to be_valid
+    end
+
     it 'should have name' do
       @product.name = nil
       expect(@product).to_not be_valid
@@ -20,6 +23,16 @@ RSpec.describe Product, type: :model do
 
     it 'should have price' do
       @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+
+    it 'should have quantity' do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+
+    it 'should have category_id' do
+      @product.category_id = nil
       expect(@product).to_not be_valid
     end
 
